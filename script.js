121364be051dbfe5cf021f06053efaddbe56842b
Soumi Pal <soumiisc2020@gmail.com>
2025-10-30 18:29:48 +0530

    change

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-hey
+let a = 10;
+let b = 3;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+let count = 5;
+console.log(count++);
+console.log(++count);
+console.log(count--);
+console.log(--count);
+
+// let age = prompt("Enter your age");
+// if (age > 18) {
+//     console.log("Adult");
+// } else {
+//     console.log("Minor");
+// }
+
+// let marks = prompt("Enter your marks:");
+// if (marks >= 90) {
+//     console.log("A grade");
+// } else if (marks >= 75) {
+//     console.log("B grade");
+// } else if (marks >= 50) {
+//     console.log("C grade");
+// } else {
+//     console.log("Fail");
+// }
+
+// let city = "Bhopal";
+// if (city === "Bhopal") {
+//     console.log("MP");
+// } else if (city === "Delhi") {
+//     console.log("Capital");
+// } else {
+//     console.log("Unknown City");
+// }
+
+// let score = 40;
+// let result = score > 35 ? "Pass" : "Fail";
+// console.log(result);
+
+// let temperature = 33;
+// let weather = temperature > 30 ? "Hot" : "Plesant";
+// console.log(weather);
+
+// switch (day) {
+//     case 1:
+//         console.log("Sunday");
+//         break;
+//     case 2:
+//         console.log("Monday");
+//         break;
+//     case 3:
+//         console.log("Tuesday");
+//         break;
+//     case 4:
+//         console.log("Wednesday");
+//         break;
+//     case 5:
+//         console.log("Thursday");
+//         break;
+//     case 6:
+//         console.log("Friday");
+//         break;
+//     case 7:
+//         console.log("Saturday");
+//         break;
+//     default:
+//         console.log("Invalid Day");
+// }
+
